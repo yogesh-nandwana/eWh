97741846993de61667080e25875cb518ea94d3ef
yogeshnandwana <yogesh.nandwana@gmail.com>
2021-05-28 17:26:57 +0530

    Fixed form bean pojo

--- src/main/java/com/na/ewh/model/LoginForm.java
+++ src/main/java/com/na/ewh/model/LoginForm.java
@@ -6,17 +6,25 @@ import javax.validation.constraints.Size;
 public class LoginForm {
 	@NotNull(message = "UserId is required!")
 	@Size(min = 4,max=10,message = "UserId should be between 4 to 10 chars!")
-	private final String userId = null;
+	private String userId = null;
 	
 	@NotNull(message = "Password is required!")
 	@Size(min = 6,max=10,message = "Password should be between 6 to 10 chars!")
-	private final String pwd = null;
+	private String pwd = null;
 
 	public String getUserId() {
 		return userId;
+	}
+	
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
 	}
 
 	public String getPwd() {
 		return pwd;
 	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}	
 }

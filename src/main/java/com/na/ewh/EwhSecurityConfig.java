21a181b08793a68736fc14b1e4e404e29843b00d
yogeshnandwana <yogesh.nandwana@gmail.com>
2021-06-02 17:28:26 +0530

    added logout link

--- src/main/java/com/na/ewh/EwhSecurityConfig.java
+++ src/main/java/com/na/ewh/EwhSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -26,12 +27,13 @@ public class EwhSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/").permitAll()
-		.antMatchers("/customers","/savecustomer").hasAnyRole("USER","ADMIN")
-		.antMatchers("/admin", "/h2-console/**").hasRole("ADMIN")
-		.and().csrf().ignoringAntMatchers("/h2-console/**","/savecustomer")//don't apply CSRF protection to /h2-console
-        .and().headers().frameOptions().sameOrigin()//allow use of frame to same origin urls
-        .and().formLogin();//enable form login instead of basic login
+			.antMatchers("/").permitAll()
+			.antMatchers("/customers","/savecustomer","/logout").hasAnyRole("USER","ADMIN")
+			.antMatchers("/admin", "/h2-console/**").hasRole("ADMIN")
+			.and().csrf().ignoringAntMatchers("/h2-console/**","/savecustomer")//don't apply CSRF protection to /h2-console
+			.and().headers().frameOptions().sameOrigin()//allow use of frame to same origin urls
+			.and().formLogin();//enable form login instead of basic login
+			//.loginPage("/login");
 	}
 
 	@Override
@@ -49,5 +51,17 @@ public class EwhSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	/**
+	 * @return currently logged in user
+	 */
+	public String currentLoggedUser() {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if (principal instanceof UserDetails) {
+			return ((UserDetails) principal).getUsername();
+		} else {
+			return principal.toString();
+		}
 	}
 }
